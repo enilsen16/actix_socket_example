81e93cba16e93aa2ae3286ab84684a0b294881a2
Erik Nilsen <enilsen16@live.com>
2021-03-19 15:45:57 +0800

    Use a unix socket instead of a tcp server

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use std::sync::{
 };
 use std::{env, io};
 
-use actix_rt::net::TcpStream;
+use actix_rt::net::UnixStream;
 use actix_server::Server;
 use actix_service::pipeline_factory;
 use bytes::BytesMut;
 use futures_util::future::ok;
 use log::{error, info};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt};
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
@@ -20,18 +20,15 @@ async fn main() -> io::Result<()> {
 
     let count = Arc::new(AtomicUsize::new(0));
 
-    let addr = ("127.0.0.1", 8080);
-    info!("starting server on port: {}", &addr.0);
-
     // Bind socket address and start worker(s). By default, the server uses the number of available
     // logical CPU cores as the worker count. For this reason, the closure passed to bind needs
     // to return a service *factory*; so it can be created once per worker.
     Server::build()
-        .bind("echo", addr, move || {
+        .bind_uds("echo", "/tmp/rust-uds.sock", move || {
             let count = Arc::clone(&count);
             let num2 = Arc::clone(&count);
 
-            pipeline_factory(move |mut stream: TcpStream| {
+            pipeline_factory(move |mut stream: UnixStream| {
                 let count = Arc::clone(&count);
 
                 async move {
